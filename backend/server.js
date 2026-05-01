f72f51f9a51c4c447e9bacaa5b5884a4bc468844
Raj <graj31105@gmail.com>
2026-05-01 22:49:39 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://task-app-fawn-pi.vercel.app"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
